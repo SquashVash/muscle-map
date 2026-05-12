63f1cef9365837b691594a09c4f07245a5e32895
shay alon vash <shayalon.vash@gmail.com>
2026-05-12 22:02:50 +0300

    Simplified example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -70,32 +70,16 @@ class _HomeViewState extends State<HomeView> {
         ],
       ),
       body: Center(
-        child: InteractiveViewer(
-          scaleEnabled: true,
-          panEnabled: true,
-          constrained: true,
-          child: Padding(
-            padding: const EdgeInsets.only(right: 30.0),
-            child: Align(
-              alignment: Alignment.center,
-              child: Transform.scale(
-                scale: 1.2,
-                child: MuscleIntensityMap(
-                  key: _mapKey,
-                  width: MediaQuery.of(context).size.width,
-                  height: MediaQuery.of(context).size.height,
-                  map: selectedMap,
-                  initialSelectedGroups: selectedMuscleGroups,
-                  onChanged: (muscles) {
-                    setState(() {
-                      selectedMuscles = muscles;
-                    });
-                  },
-                  strokeColor: Colors.black,
-                ),
-              ),
-            ),
-          ),
+        child: MuscleIntensityMap(
+          key: _mapKey,
+          map: selectedMap,
+          initialSelectedGroups: selectedMuscleGroups,
+          onChanged: (muscles) {
+            setState(() {
+              selectedMuscles = muscles;
+            });
+          },
+          strokeColor: Colors.black,
         ),
       ),
     );
